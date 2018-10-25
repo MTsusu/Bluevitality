976f86421b3e91f8c4b3f7c992381ffd615639dd
Bluevitality <19450673+Bluevitality@users.noreply.github.com>
2018-10-25 09:43:40 +0800

    Update 日志内存溢出关键字扫描并重启.sh

--- Linux/Web/Tomcat/Setup JMX And Optimization/日志内存溢出关键字扫描并重启.sh
+++ Linux/Web/Tomcat/Setup JMX And Optimization/日志内存溢出关键字扫描并重启.sh
@@ -33,23 +33,23 @@ function server_start() {
 
 for log in $LOGS
 {
-    ECPCORE_NEW_NUM=`echo $log | grep -oP '.*(?=/logs)' | awk -F'/' '{print $NF}'`    #从日志路径截取出节点名部分    #替换了旧的：| awk -F"[_/]" '{print $7"_"$8}'
-    [[ -f $COUNT_LOG_PATH/${ECPCORE_NEW_NUM}.log-${DATE} ]] || echo 1 > $COUNT_LOG_PATH/${ECPCORE_NEW_NUM}.log-${DATE} 
+    NODE_NAME=`echo $log | grep -oP '.*(?=/logs)' | awk -F'/' '{print $NF}'`    #从日志路径截取出节点名部分    #替换了旧的：| awk -F"[_/]" '{print $7"_"$8}'
+    [[ -f $COUNT_LOG_PATH/${NODE_NAME}.log-${DATE} ]] || echo 1 > $COUNT_LOG_PATH/${NODE_NAME}.log-${DATE} 
     
-    LINE_NUM=`cat $COUNT_LOG_PATH/${ECPCORE_NEW_NUM}.log-${DATE}`   #取出扫描的上次结束行位置
+    LINE_NUM=`cat $COUNT_LOG_PATH/${NODE_NAME}.log-${DATE}`   #取出扫描的上次结束行位置
     ALL_LINE=`wc -l $log | awk '{print $1}'`                        #取出日志文件总行数
     
     echo $log
     echo $ALL_LINE
     GC_COUNT=`sed -n "$LINE_NUM,${ALL_LINE}p" $log |grep -sc "${GC_ERROR}"`   #从特定日志行范围内扫描内存溢出关键字的次数
-    echo $ALL_LINE > $COUNT_LOG_PATH/${ECPCORE_NEW_NUM}.log-${DATE}         #记录本次执行扫描时结束的行数
+    echo $ALL_LINE > $COUNT_LOG_PATH/${NODE_NAME}.log-${DATE}         #记录本次执行扫描时结束的行数
     
     if [ $GC_COUNT -gt 0 ]; then
-        echo "restart $ECPCORE_NEW_NUM"
-        ECPCORE_NEW_PID=`ps aux | grep $ECPCORE_NEW_NUM | egrep -v "grep|tail" | awk '{print$2}'`
-        $HOME/jdk7/bin/jmap -dump:format=b,file=$COUNT_LOG_PATH/${ECPCORE_NEW_NUM}_$(date "+%F_%H%M%S")_hprof  $ECPCORE_NEW_PID
-        server_stop $ECPCORE_NEW_NUM
-        server_start $ECPCORE_NEW_NUM
+        echo "restart $NODE_NAME"
+        ECPCORE_NEW_PID=`ps aux | grep $NODE_NAME | egrep -v "grep|tail" | awk '{print$2}'`
+        $HOME/jdk7/bin/jmap -dump:format=b,file=$COUNT_LOG_PATH/${NODE_NAME}_$(date "+%F_%H%M%S")_hprof  $ECPCORE_NEW_PID
+        server_stop $NODE_NAME
+        server_start $NODE_NAME
     fi
 } 
 
