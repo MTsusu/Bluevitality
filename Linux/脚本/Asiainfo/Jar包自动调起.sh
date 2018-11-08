d9a750fe223ee7318a8ff62cc9217d130cc901d7
bluevitality <inmoonlight@163.com>
2018-11-08 10:22:03 +0800

    scripts

--- Linux/脚本/Asiainfo/Jar包自动调起.sh
+++ Linux/脚本/Asiainfo/Jar包自动调起.sh
@@ -4,23 +4,23 @@ source ~/.bash_profile
 #-------------------------------
 #此脚本适用于"java -jar"方式的自动调起
 #启动/关闭脚本内必须是1个节点对应1行命令的方式，用于"grep"匹配节点名对应的执行命令进行执行!!
+#如需要修改启停脚本的绝对路径，请修改第34、35行，默认：$HOME/sbin/{start/stop}-${PROJECT_NAME}.sh
 #-------------------------------
 
-#工程目录名（第1参数）,ps工程名和在此目录下查找JAR包用
-PROJECT_NAME="iscadapi-core"
-#JAR包名（第2参数）及节点数
-APPLICATION="logger-admin-service-impl-1.0.0-SNAPSHOT.jar"
-NODE_NUMBER=4
-#启停脚本路径
-START_APP_SCRIPT_PATH="$HOME/sbin/start-${PROJECT_NAME}.sh"
-STOP_APP_SCRIPT_PATH="$HOME/sbin/stop-${PROJECT_NAME}.sh"
+# #工程目录名字（第1参数）,ps工程名和在此目录下查找JAR包用
+# PROJECT_NAME="iscadapi-core"
+# #JAR包名字（第2参数）及节点数
+# APPLICATION="logger-admin-service-impl-1.0.0-SNAPSHOT.jar"
+# NODE_NUMBER=4
 
 #若未定义则使用传参：脚本 工程名 JAR包名 启动脚本路径 停止脚本路径
-[ -z $PROJECT_PATH ] && PROJECT_NAME=$1                     #工程名称
+[ -z $PROJECT_NAME ] && PROJECT_NAME=$1                     #工程名称
 [ -z $APPLICATION ] && APPLICATION=$2                       #JAR包名称
-[ -z $NODE_NUMBER ] && NODE_NUMBER=$3                       #JAR包节点数           
-[ -z $START_APP_SCRIPT_PATH ] && START_APP_SCRIPT_PATH=$4   #启动脚本路径
-[ -z $STOP_APP_SCRIPT_PATH ] && STOP_APP_SCRIPT_PATH=$5     #停止脚本路径
+[ -z $NODE_NUMBER ] && NODE_NUMBER=$3                       #JAR包节点数  
+         
+[ -z $PROJECT_NAME ] && exit 1
+[ -z $APPLICATION ] &&  exit 1
+[ -z $NODE_NUMBER ] &&  exit 1
 
 #取当前时间和JAR包时间戳（yyyy-mm-dd HH）
 DATATIME_NOW=$(date "+%F %H:%M")
@@ -34,8 +34,7 @@ function START_STOP() {
     grep $i $HOME/sbin/start-${PROJECT_NAME}.sh > $HOME/sbin/Node_start.sh
     grep $i $HOME/sbin/stop-${PROJECT_NAME}.sh  > $HOME/sbin/Node_stop.sh
     
-    sh $HOME/sbin/Node_stop.sh >> $HOME/sbin/node_pullup.log
-    sleep 2
+    sh $HOME/sbin/Node_stop.sh >> $HOME/sbin/node_pullup.log ; sleep 2
     nohup sh $HOME/sbin/Node_start.sh &> /dev/null &
     echo "${DATATIME_NOW} $i 节点不存在，正在启动" >> $HOME/sbin/node_pullup.log
     rm $HOME/sbin/Node_start.sh $HOME/sbin/Node_stop.sh
@@ -58,4 +57,3 @@ else
         }
     fi   
 fi
-
