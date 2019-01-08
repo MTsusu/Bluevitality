1db844610dcd82f3dd51ce0862936b07daba3ecb
Bluevitality <19450673+Bluevitality@users.noreply.github.com>
2019-01-08 16:20:43 +0800

    Update 日志按天分割及部分优化.sh

--- Linux/WEB/Nginx/日志按天分割及部分优化.sh
+++ Linux/WEB/Nginx/日志按天分割及部分优化.sh
@@ -47,3 +47,76 @@ cat > ~/nginx/conf/proxy_header.conf <<'eof'
   proxy_set_header REMOTE_ADD $remote_addr;
   proxy_redirect http:// $scheme://;
 eof
+
+
+cat > ~/nginx/conf/nginx.conf <<'eof'
+worker_processes  32;
+pid  logs/nginx.pid;
+events {
+    worker_connections  1024000;
+    use epoll;
+}
+
+http {
+    include       mime.types;
+    default_type  application/octet-stream;
+
+    set_real_ip_from 192.168.0.0/16;
+    real_ip_header X-Forwarded-For;
+
+    log_format main  '$time_local || $remote_addr || $upstream_addr ||  $status || $request_time || $upstream_status || $upstream_response_time'
+                     ' || $upstream_cache_status || $body_bytes_sent || $http_referer'
+                     ' || $remote_user || $http_user_agent || $http_x_forwarded_for || $request';
+
+
+    sendfile        on;
+    keepalive_timeout  15;
+    server_names_hash_bucket_size 128;
+    tcp_nopush on;   
+    tcp_nodelay on; 
+    server_tokens off;
+    charset utf8;
+
+    proxy_cache cache_one;
+    proxy_cache_path tmp/proxy_cache levels=1:2 keys_zone=cache_one:100m inactive=2d max_size=1g;
+    proxy_next_upstream off;
+    proxy_read_timeout 300;
+    proxy_send_timeout 300;
+    proxy_buffer_size 16k;
+    proxy_buffers 4 16k;
+    proxy_busy_buffers_size 32k;
+    proxy_temp_file_write_size 32k;
+    proxy_temp_path tmp/proxy_temp;
+    proxy_ignore_client_abort on;
+
+    client_header_buffer_size   2k;
+    large_client_header_buffers 4 16k;
+    client_max_body_size 100m;
+    
+    gzip on;
+    gzip_min_length     2k;
+    gzip_buffers        4       16k;
+    gzip_comp_level     2;
+    gzip_types       text/js image/jpeg image/png text/plain text/css text/javascript application/json application/javascript;
+    gzip_vary on;
+    gzip_proxied  expired no-cache no-store private auth;
+
+    include conf.d/vhost/app/*.conf;
+    include conf.d/upstream/app/*-upstream.conf;
+}
+
+   stream {
+     upstream proxy_guizhou {
+        server XX.XX.XX.XX:XXX;
+        #check interval=3000 rise=2 fall=5 timeout=1000;
+        #check_http_send "GET /HTTP/1.0\r\n\r\n";
+        #check_http_expect_alive http_2xx http_3xx;
+    }
+    server {
+        listen 10000;
+        proxy_pass proxy_guizhou;
+    }
+       
+        include conf.d/tcp/*-tcp.conf;
+}
+eof
