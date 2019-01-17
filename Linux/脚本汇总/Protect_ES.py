9cb8b15f84c4f4661dd62cfd0a6682827e18d2b8
Bluevitality <19450673+Bluevitality@users.noreply.github.com>
2019-01-18 02:50:53 +0800

    Create Protect_ES.py

---
+++ Linux/脚本汇总/Protect_ES.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python             
+# -*- coding: UTF-8 -*-
+  
+import os
+import multiprocessing
+import time
+
+from multiprocessing import cpu_count
+CPU_NUMBERS=cpu_count() #CPU核心数量
+SECURITY_LOAD_LEVEL=float(CPU_NUMBERS * 0.5)  #保持在负载安全阈值
+
+print "this cpu numbers:" + str(CPU_NUMBERS)  #主机CPU数
+print "my pid:" + str(os.getpid())            #PID
+
+#获取主机1分钟负载信息
+def get_load():
+    f = open("/proc/loadavg")
+    loadstate=f.read().split()
+    return loadstate[0]
+
+#跑起来
+def Oh(x):
+    while True:
+        if float(get_load()) >= SECURITY_LOAD_LEVEL:  #当负载高于安全阈值执行Sleep
+            time.sleep(0.01)
+            continue
+        print "cpu numbers:" + str(float(CPU_NUMBERS * 0.5))
+        print "Load" + str(get_load())
+        pass    #空转
+
+if __name__ == '__main__':
+    record = []
+    for i in range(int(CPU_NUMBERS)): #Fork出与CPU核心数相同的进程开始吃CPU
+        p=multiprocessing.Process(target=Oh,args=(str(i)))
+        p.start()
+        record.append(p)
+    for i in record:
+        p.join()
